d726c007d7eb2d27466459f4fca6eb54feef054a
IrinaTentina <irina_tentina@mail.ru>
2015-10-12 17:12:48 +0300

    project

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public class market
+    {
+        public int n; // число покупателей конкретного товара на рынке при определенных условиях, ед.
+        public int k; //количество покупок среднего потребителя за некоторый период (за год), ед. в год
+        public int p; // средняя цена за единицу товара, руб
+        public int Q; // итоговый показатель - рыночный потенциал спроса
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите показатели:");
+            Console.WriteLine("Число покупателей Вашего веб-сервиса за год:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Число покупок Вашего веб-сервиса за год:");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Средняя цена Вашей единицы товара:");
+            int p = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+    }
+}
